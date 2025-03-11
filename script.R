a83b8690dc325ebf83dd7d78ebd467c761efbb54
Hariaksha <hariakshagunda@gmail.com>
2025-03-11 12:35:47 +0700

    progress 11 maret

--- script.R
+++ script.R
@@ -8,6 +8,5 @@ setwd("C:/Repositories/magang-ypi")
 
 # Read all sheets
 sheets = excel_sheets("YPI Data.xlsx")
-data = read_excel("YPI Data.xlsx", sheet = "Keberadaan Bank", range = "A1:E41")
-results = quantile(data["Tidak ada bank"], probs = c(0,0.25,0.5,0.75,1))
+data = read_excel("YPI Data.xlsx", sheet = "Indeks")
 metrik = data[,5]
